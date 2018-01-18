07e077ad52a3da63dae4e85585397968a0aecc6a
Nicolas Klein <nicolasklein1998@gmail.com>
2018-01-18 19:02:17 +0100

    Meine Tollen änderungen
    yeah
    
    yeah yeah yeaaaaah
    *FirstBug
    *SecondBUg

--- uTestProgramm/meinStuff.java
+++ uTestProgramm/meinStuff.java
@@ -2,6 +2,10 @@ public class meinStuff{
 
 	private String myName = "Nico";
 
+/**
+Forget to comment suff yeah
+**/
+
 	public void main (String[]args){
 		System.out.println("Hello World" + myName + "basic shit");
 	}

188cc766f1312086191f15990f633e7009cc4d1d
Nicolas Klein <nicolasklein1998@gmail.com>
2018-02-24 15:45:17 +0100

    u12 Update

--- u10ProgrammCollection/src/MetaQueue.java
+++ u10ProgrammCollection/src/MetaQueue.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -55,6 +56,83 @@ public class MetaQueue<E> implements Queue<E>, Iterable<E>{
     @Override
     public int size() {
         return queue.size();
+    }
+
+    /**
+     * Ausgabe der Queue in eine vom
+     * Benutzer wählbare Datei
+     */
+
+    public void serialize(String dat)throws IOException{
+        File datei = new File(dat);
+        ObjectOutputStream out = new ObjectOutputStream(
+                                 new BufferedOutputStream(
+                                 new FileOutputStream(datei)));
+        out.writeObject(queue);
+        out.close();
+    }
+
+    /**
+     * Einlesen der Queue aus einer vom
+     * Benutzer wählbaren Datei
+     *
+     * TODO CHECK CAST 
+     */
+
+    public void deserialize(String dat) throws IOException, ClassNotFoundException{
+        File datei = new File(dat);
+        ObjectInputStream in = new ObjectInputStream(
+                               new BufferedInputStream(
+                               new FileInputStream(datei)));
+        LinkedList<E> objQueue = (LinkedList<E>) in.readObject();
+    }
+
+    /**
+     * Pseudo clone Method die eine LinkedList
+     * gefüllt mit den Werten der Queue zurück gibt.
+     * @return  Kopie der LinkedList queue mit Werten.
+     */
+
+    public LinkedList<E> clone(){
+        LinkedList<E> listClone = new LinkedList<>();
+        for (E e : queue){
+            listClone.add(e);
+        }
+        return listClone;
+    }
+
+    /**
+     * Fügt plusListe ans Ende der queue an.
+     * Vorraussetzung ist das sowohl die Queue
+     * als auch die plusList den selben Typ haben.
+     *
+     * @param plusList  anzufügende Liste.
+     */
+
+    public void append(LinkedList<E> plusList){
+        for (E e: plusList){
+            queue.add(e);
+        }
+    }
+
+    /**
+     * Überprüft zwei LinkedLists von Typ E auf
+     * inhaltliche Gleicheit.
+     * @param   list    mit queue zu vergleichende Liste
+     * @return  true    inhaltlich gleich
+     * @return  false   inhaltlich nicht gleich
+     */
+
+    public boolean equals(LinkedList<E> list) {
+        Iterator<E> it1 = list.iterator();
+        Iterator<E> it2 = queue.iterator();
+        
+        while(it1.hasNext()){
+            if ((it1.next() != it2.next() || (!it2.hasNext()))){
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
